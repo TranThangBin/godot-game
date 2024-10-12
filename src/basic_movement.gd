81c7591c15a368c4282a210ff24df9076b8657c1
TranThangBin <thangdev04@gmail.com>
2024-10-12 12:31:41 +0700

    feat: coyote jump and remove [jump buffer](will think about it later)

--- src/basic_movement.gd
+++ src/basic_movement.gd
@@ -8,15 +8,24 @@ const JUMP_VELOCITY = -400.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+@onready var was_on_floor = is_on_floor()
+@onready var coyote_timer = $CoyoteTimer
+
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
-
-	# Handle jump.	
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	
+	if was_on_floor and !is_on_floor() and velocity.y > 0:
+		coyote_timer.start()
+	
+	was_on_floor = is_on_floor()
+	
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and (is_on_floor() or !coyote_timer.is_stopped()):
 		velocity.y = JUMP_VELOCITY
-
+		coyote_timer.stop()
+	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
--- src/hud.gd
+++ src/hud.gd
@@ -42,7 +42,7 @@ func _on_settings_menu_save():
 	return_from_settings()
 
 func _on_main_menu_button_pressed():
-	get_tree().change_scene_to_file("res://scene/main_menu.tscn")
+	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
 
 func _on_quit_button_pressed():
 	get_tree().quit(0)
