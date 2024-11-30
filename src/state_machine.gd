0445ae13bb534bdfa2561db24c781e5af84a532e
TranThangBin <thangdev04@gmail.com>
2024-11-30 15:39:47 +0700

    chore: level 2 is close to finish

--- src/state_machine.gd
+++ src/state_machine.gd
@@ -2,23 +2,20 @@ class_name StateMachine
 
 extends Node
 
-@export var _debug_label: Label
 @export var _initial_state: State
 var _current_state: State
 var _states := {}
 
 
 func _ready():
-	for child: State in get_children():
+	for child in get_children():
 		if child is State:
-			var state: State = child
+			var state = child as State
 			_states[state.name.to_lower()] = child
 			state.transition.connect(_on_child_transition)
 	if _initial_state != null:
 		_initial_state.enter()
 		_current_state = _initial_state
-		if _debug_label != null:
-			_debug_label.set_text(_current_state.name)
 
 
 func _process(delta):
@@ -32,9 +29,6 @@ func _physics_process(delta):
 
 
 func _on_child_transition(state: State, new_state_name: String):
-	if _debug_label != null:
-		_debug_label.set_text(new_state_name)
-
 	if state != _current_state:
 		return
 
